37da6f34abc6152bd73360afd393e4c499f54b8b
Abhishek-S-Mahajan <abhishekmahajan646@yahoo.com>
2025-01-10 21:07:05 +0530

    getURLs in the HTML web page and the correlated tests

--- crawl-website.js
+++ crawl-website.js
@@ -1,14 +1,73 @@
+// @desc: since we don't have access to the DOM in the node environment
+const { JSDOM } = require("jsdom");
+
+// @desc: get all the URLs present in the given HTML web page and return an array of URL strings
+function getURLs(htmlBody, baseURL) {
+    const urls = [];
+
+    // @desc: creates DOM tree structure from the HTML string
+    const domObject = new JSDOM(htmlBody);
+
+    // @desc: returns a node list of all link elements
+    const linkElementsList = domObject.window.document.querySelectorAll("a");
+
+    linkElementsList.forEach(({ href }) => {
+
+
+        if (href.slice(0, 1) === "/") {
+            try {
+
+                // @desc: this a relative URL
+                const urlObject = new URL(`${baseURL}${href}`);
+                urls.push(urlObject.href);
+
+            } catch (error) {
+                console.error("Error with Relative URL:", error.message);
+
+            }
+
+        } else {
+            try {
+
+                // @desc: this is an absolute URL
+                const urlObject = new URL(href);
+                urls.push(urlObject.href);
+
+            } catch (error) {
+                console.error("Error with Absolute URL:", error.message);
+
+            }
+        }
+
+    });
+
+
+    return urls;
+}
+
+
+
+// @desc: normalize the URL of the website
 function normalizeURL(urlString) {
     const urlObject = new URL(urlString);
     const { hostname, pathname } = urlObject;
     const hostPath = `${hostname}${pathname}`;
     if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
-        // everything except the last character
+        // @desc: everything except the last character
         return hostPath.slice(0, -1);
     }
     return hostPath;
 }
 
+
+
+
+
+
+
+
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLs
 }
--- crawl-website.test.js
+++ crawl-website.test.js
@@ -1,16 +1,23 @@
-const { normalizeURL } = require("./crawl-website.js");
+const { normalizeURL, getURLs } = require("./crawl-website.js");
 const { test, expect } = require("@jest/globals");
 
-// @desc: get rid of the protocol
-test("normalizeURL strip the protocol", () => {
+// @desc: get rid of the "https://" protocol
+test("normalizeURL strip the https protocol", () => {
     const input = "https://blog.boot.dev/path";
     const actualOutput = normalizeURL(input);
     const expectedOutput = "blog.boot.dev/path";
     expect(actualOutput).toEqual(expectedOutput);
 });
 
+// @desc: get rid of the "http://" protocol
+test("normalizeURL strip the https protocol", () => {
+    const input = "http://blog.boot.dev/path";
+    const actualOutput = normalizeURL(input);
+    const expectedOutput = "blog.boot.dev/path";
+    expect(actualOutput).toEqual(expectedOutput);
+});
 
-// @desc: get rid of the trailing backslash
+// @desc: get rid of the trailing backslash at the end of the url
 test("normalizeURL strip the last backslash", () => {
     const input = "https://blog.boot.dev/path/";
     const actualOutput = normalizeURL(input);
@@ -25,3 +32,66 @@ test("normalizeURL take care of the capitals", () => {
     const expectedOutput = "blog.boot.dev/path";
     expect(actualOutput).toEqual(expectedOutput);
 });
+
+// @desc: get all the URLs present in the HTML web page and return an array of URL strings
+// @desc: Absolute URL
+test("getAbsoluteURL in the HTML", () => {
+    const htmlBodyInput = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">Blogs</a>
+        </body>
+    </html>
+    `;
+    const baseURLInput = "https://blog.boot.dev";
+    const actualOutput = getURLs(htmlBodyInput, baseURLInput);
+    const expectedOutput = ["https://blog.boot.dev/path/"];
+    expect(actualOutput).toEqual(expectedOutput);
+});
+
+// @desc: Relative URL
+test("getRelativeURL in the HTML", () => {
+    const htmlBodyInput = `
+    <html>
+        <body>
+            <a href="/path/">Blogs</a>
+        </body>
+    </html>
+    `;
+    const baseURLInput = "https://blog.boot.dev";
+    const actualOutput = getURLs(htmlBodyInput, baseURLInput);
+    const expectedOutput = ["https://blog.boot.dev/path/"];
+    expect(actualOutput).toEqual(expectedOutput);
+});
+
+// @desc: multiple URLs (both relative and absolute)
+test("getMultipleURLs in the HTML", () => {
+    const htmlBodyInput = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/">Blogs Path 1 (Absolute Link)</a>
+            <a href="/path2/">Blogs Path 2 (Relative Link)</a>
+        </body>
+    </html>
+    `;
+    const baseURLInput = "https://blog.boot.dev";
+    const actualOutput = getURLs(htmlBodyInput, baseURLInput);
+    const expectedOutput = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"];
+    expect(actualOutput).toEqual(expectedOutput);
+});
+
+// @desc: exclude invalid URL
+test("excludeInvalidURLs in the HTML", () => {
+    const htmlBodyInput = `
+    <html>
+        <body>
+            <a href="invalid">Invalid URL Link</a>
+        </body>
+    </html>
+    `;
+    const baseURLInput = "https://blog.boot.dev";
+    const actualOutput = getURLs(htmlBodyInput, baseURLInput);
+    const expectedOutput = [];
+    expect(actualOutput).toEqual(expectedOutput);
+});
+
